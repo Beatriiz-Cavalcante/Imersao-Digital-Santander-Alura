3cb7de7b26dc3d46ba6c760225e70d54888a65b3
Beatriiz-Cavalcante <biamenezes193@gmail.com>
2025-09-30 16:31:05 -0300

    lógica com js

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 29;
+let chute = prompt ('Escolha um número entre 1 e 30');
+
+while (chute != numeroSecreto){
+    if(chute != numeroSecreto){
+        chute = prompt ('Errado! Tente outra vez');
+    } 
+};
+alert('Você acertou!');
