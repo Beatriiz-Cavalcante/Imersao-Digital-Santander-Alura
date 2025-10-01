7197c5f10365dfe4ddd6a32847f7caac2a171c05
Beatriiz-Cavalcante <biamenezes193@gmail.com>
2025-10-01 16:26:09 -0300

    método Math eoperador ternário

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,32 @@
 alert('Boas-vindas ao jogo do número secreto');
-let numeroSecreto = 29;
-let chute = prompt ('Escolha um número entre 1 e 30');
+let numMax = 10
+let numeroSecreto = parseInt(Math.random() * numMax + 1); 
+console.log(numeroSecreto);
+chute = prompt (`Escolha um número entre 1 e ${numMax}`);
+let tentativa = 1;
 
 while (chute != numeroSecreto){
     if(chute != numeroSecreto){
-        chute = prompt ('Errado! Tente outra vez');
+        if (chute > numeroSecreto){
+            chute = prompt(`O número secreto é menor que ${chute}`);
+        } else {
+            chute = prompt(`O número secreto é maior que ${chute}`);
+        }
     }
-    console.log('entrou no while'); 
+    console.log('entrou no while');
+    tentativa = tentativa +1; 
 };
-alert(`Você acertou! ${numeroSecreto} é o número`);
+
+if (tentativa > 1) {
+    alert(`Você acertou após ${tentativa} tentativa! o número secreto é ${numeroSecreto}`);
+} else {
+    alert(`Você acertou após ${tentativa} tentativas! o número secreto é ${numeroSecreto}`);
+}
+
+//operador ternário 
+// let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa'
+// outra forma de escrever a ultima estrututa condicional
+
 console.log('saiu do while');
+
+// Operadoreos lógicos:     e = &&   e   ou = ||
