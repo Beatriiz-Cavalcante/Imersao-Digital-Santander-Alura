9b2a1a271744affe6f0d117d14c1a5832a3f893e
Beatriiz-Cavalcante <biamenezes193@gmail.com>
2025-10-01 09:49:49 -0300

    relembrando concatenação

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -5,6 +5,8 @@ let chute = prompt ('Escolha um número entre 1 e 30');
 while (chute != numeroSecreto){
     if(chute != numeroSecreto){
         chute = prompt ('Errado! Tente outra vez');
-    } 
+    }
+    console.log('entrou no while'); 
 };
-alert('Você acertou!');
+alert(`Você acertou! ${numeroSecreto} é o número`);
+console.log('saiu do while');
